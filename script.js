735e25f282bd7728f0646fd4d85cfb089bde394a
annabellesanok <annabellejoranis@gmail.com>
2024-04-15 21:28:46 -0400

    updates and added python notebook file

--- script.js
+++ script.js
@@ -1,108 +1,120 @@
-//setting dimensions and margins
 var margin = {top: 10, right: 30, bottom: 50, left: 70}, 
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 500 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
 
-//load csv file
-d3.csv("aggregated_2022.csv").then(function(data) {
+// Append the svg object to the body of the page
+var svg = d3.select("#bubblechart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    //add svg object
-    var svg = d3.select("#scatterplot").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
-    
+// Read the data from tech.csv
+d3.csv("tech.csv").then(function(data) {
 
-    //add x axis
-    var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return +d.pe_ratio; }))
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  data.forEach(function(d) {
+    d.pe_ratio = +d.pe_ratio;
+    d.pfcf_ratio = +d.pfcf_ratio;
+    d.return_on_equity = +d.return_on_equity;
+  });
 
-    //text label for x axis
-    svg.append("text")             
-      .attr("transform",
-            "translate(" + (width/2) + " ," + 
-                           (height + margin.top + 20) + ")")
-      .style("text-anchor", "middle")
-      .text("PE Ratio");
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d.pe_ratio; })])
+    .range([0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-    //add y axis
-    var y = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return +d.pfcf_ratio; }))
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d.pfcf_ratio; })])
+    .range([height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-    //text label for y axis
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("PFCF Ratio");  
+  // Add a scale for bubble size
+  var z = d3.scaleSqrt()
+    .domain([0, d3.max(data, function(d) { return d.return_on_equity; })])
+    .range([2, 30]); // Bubble size
 
-    //making tooltip
-    var tooltip = d3.select("body").append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "white")
-      .style("border", "solid")
-      .style("border-width", "2px")
-      .style("border-radius", "5px")
-      .style("padding", "5px");
+  // Add a tooltip div. Invisible by default.
+  var tooltip = d3.select("#bubblechart")
+    .append("div")
+    .attr("class", "tooltip");
 
-    var mouseover = function(event, d) {
-      tooltip
-        .style("opacity", 1);
-    };
-
-    var mousemove = function(event, d) {
-      tooltip
-        .html("Ticker: " + d.Ticker + "<br>PE Ratio: " + d.pe_ratio + "<br>PFCF Ratio: " + d.pfcf_ratio)
-        .style("left", (event.x) + "px")
-        .style("top", (event.y) + "px");
-    };
-
-    var mouseleave = function(event, d) {
-      tooltip
+  // Add bubbles
+  svg.selectAll(".bubble")
+    .data(data)
+    .enter().append("circle")
+    .attr("class", "bubble")
+    .attr("cx", function(d) { return x(d.pe_ratio); })
+    .attr("cy", function(d) { return y(d.pfcf_ratio); })
+    .attr("r", function(d) { return z(d.return_on_equity); }) 
+    .style("fill", "#69b3a2")
+    .on("mouseover", function(event, d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+      tooltip.html("Ticker: " + d.Ticker + "<br/>PE Ratio: " + d.pe_ratio + "<br/>P/FCF Ratio: " + d.pfcf_ratio + "<br/>ROE: " + d.return_on_equity)
+        .style("left", (event.pageX) + "px")
+        .style("top", (event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+      tooltip.transition()
+        .duration(500)
         .style("opacity", 0);
-    };
+    });
 
-   //adding dots and labels
-svg.append('g')
-.selectAll("dot")
-.data(data)
-.enter()
-.append("circle")
-  .attr("cx", function (d) { return x(+d.pe_ratio); })
-  .attr("cy", function (d) { return y(+d.pfcf_ratio); })
-  .attr("r", 6) 
-  .style("fill", "blue")
-  .style("opacity", 0.7)
-  .on("mouseover", mouseover)
-  .on("mousemove", mousemove)
-  .on("mouseleave", mouseleave);
+    function createLegend() {
+      // Example ROE values to show in the legend
+      var exampleROEValues = [25, 50, 100];
+  
+      // Create a group for the legend
+      var legend = svg.append("g")
+        .attr("transform", "translate(" + (width - 70) + ", 30)"); // Position the legend
+  
+      // Append circles to legend
+      legend.selectAll("legendCircles")
+        .data(exampleROEValues)
+        .enter()
+        .append("circle")
+          .attr("cy", function(d, i) { return i * 45; }) 
+          .attr("r", function(d) { return z(d); }) 
+          .style("fill", "none")
+          .style("stroke", "#555");
+  
+      // Append text to legend
+      legend.selectAll("legendLabels")
+        .data(exampleROEValues)
+        .enter()
+        .append("text")
+          .attr("y", function(d, i) { return i * 40; })
+          .attr("x", function(d) { return z(d) + 10; }) 
+          .text(function(d) { return d + "% ROE"; }) 
+          .style("font-size", "12px")
+          .attr("alignment-baseline", "middle");
+  }
+  
+  createLegend();
 
-//adding labels for each dot
-svg.append('g')
-.selectAll("text")
-.data(data)
-.enter()
-.append("text")
-  .attr("x", function(d) { return x(+d.pe_ratio); })
-  .attr("y", function(d) { return y(+d.pfcf_ratio); })
-  .attr("dx", 8) // to position the text a bit to the right of the dot
-  .attr("dy", -10) // to position the text a bit above the dot
-  .text(function(d) { return d.Ticker; })
-  .attr("font-size", "10px")
-  .attr("fill", "black");
+  // Labels for the x-axis and y-axis
+  svg.append("text")             
+    .attr("transform",
+          "translate(" + (width/2) + " ," + 
+                         (height + margin.top + 20) + ")")
+    .style("text-anchor", "middle")
+    .text("PE Ratio");
 
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("PFCF Ratio");
 }).catch(function(error){
-    console.error('Error loading the CSV file', error);
+  console.error('Error loading the CSV file', error);
 });
+
